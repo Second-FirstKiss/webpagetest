ed59ac432da9e1e16af5b001efe585f6d1b0a1dd
Second_Firstkiss <51329755+Second-FirstKiss@users.noreply.github.com>
2024-04-04 09:53:11 +0900

    add lesson 5

---
+++ 5 lesson/evtcallback.js
@@ -0,0 +1,5 @@
+const coffee = function() {
+    console.log('주문하신 바닐라 라떼 나왔습니다.');
+}
+
+setTimeout(coffee, 3000); //주문하신 바닐라 라떼 나왔습니다. (3초 후 출력)
---
+++ 5 lesson/global.js
@@ -0,0 +1,10 @@
+var savedUser = '응애';
+function account(userID)
+{
+    console.log('반갑습니다. '+savedUser+'님');
+    
+    savedUser = '응애2';
+
+}
+account();
+console.log('또 오셨네요. '+savedUser);
